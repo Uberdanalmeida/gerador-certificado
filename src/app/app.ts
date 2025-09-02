f92724909429770a4aba6ef68bd033b0424d551b
Uberdanalmeida <105062084+Uberdanalmeida@users.noreply.github.com>
2025-09-01 23:41:37 -0300

    Criando meu componente primaryButton para o botão,

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./_components/navbar/navbar";
+import { PrimaryButton } from './_components/primary-button/primary-button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, PrimaryButton],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('gerador-certificado');
+  exibeNavbar: boolean = true
 }
