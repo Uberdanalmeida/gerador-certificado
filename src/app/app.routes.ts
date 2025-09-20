f622974e0c714cae48f2548d7224698e281b25c2
Uberdanalmeida <105062084+Uberdanalmeida@users.noreply.github.com>
2025-09-19 22:10:32 -0300

    Criando rotas na aplicação.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Certificados } from './pages/certificados/certificados';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: "",
+    component: Certificados
+  }
+];
